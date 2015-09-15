6b2795a692d02829edbdc12eafb0a8ed54bcad4c
fabs <fyamagu@gwdg.de>
2015-09-15 12:17:48 +0200

    Disassembly: separate handling of comments and instructions

--- src/inputModules/radare/DisassemblyParser.java
+++ src/inputModules/radare/DisassemblyParser.java
@@ -19,18 +19,41 @@ public class DisassemblyParser
 
 	private void parseLines(ParsedDisassembly retval)
 	{
-		Object thisLine;
-		while ((thisLine = nextLine()) != null)
+		String line;
+		while ((line = nextLine()) != null)
 		{
-
+			if (isLineInstruction(line))
+				handleInstruction(retval, line);
+			else if (isLineComment(line))
+				handleComment(retval, line);
 		}
 	}
 
-	private Object nextLine()
+	private boolean isLineInstruction(String line)
+	{
+		return line.startsWith("0x");
+	}
+
+	private boolean isLineComment(String line)
+	{
+		return line.startsWith(";");
+	}
+
+	private void handleComment(ParsedDisassembly retval, String line)
+	{
+		// TODO
+	}
+
+	private void handleInstruction(ParsedDisassembly retval, String line)
+	{
+		// TODO
+	}
+
+	private String nextLine()
 	{
 		if (currentLine == lines.length)
 			return null;
-		return lines[currentLine++];
+		return lines[currentLine++].trim();
 	}
 
 	private void initializeLines(String disassembly) throws EmptyDisassembly
