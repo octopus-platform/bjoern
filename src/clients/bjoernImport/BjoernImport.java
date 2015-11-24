1d0b4f1674c16e251895d282c76aeaee8746ab67
fabs <fyamagu@gwdg.de>
2015-11-24 14:52:52 +0100

    Provide useful error messages.

--- src/clients/bjoernImport/BjoernImport.java
+++ src/clients/bjoernImport/BjoernImport.java
@@ -1,6 +1,7 @@
 package clients.bjoernImport;
 
 import java.io.IOException;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -27,40 +28,51 @@ public class BjoernImport
 	private static void invokeRadare2(String[] args) throws IOException
 	{
 		String pathToBinary = cmdLine.getCodedir();
+		System.out.println("Running radare on " + pathToBinary);
 		RadareExporter.export(pathToBinary, ".");
 	}
 
 	private static void invokeImportPlugin() throws MalformedURLException
 	{
 
+		String workingDirectory = System.getProperty("user.dir");
+
+		Path nodePath = Paths.get(workingDirectory, "nodes.csv");
+		String nodeFilename = URLEncoder.encode(nodePath.toAbsolutePath()
+				.toString());
+
+		Path edgePath = Paths.get(workingDirectory, "edges.csv");
+		String edgeFilename = URLEncoder.encode(edgePath.toAbsolutePath()
+				.toString());
+
+		String dbName = URLEncoder.encode(cmdLine.getDbName());
+
+		String urlStr = String.format(
+				"http://localhost:2480/importcsv/%s/%s/%s/", nodeFilename,
+				edgeFilename, dbName);
+
+		URL url = new URL(urlStr);
+		HttpURLConnection connection;
+
 		try
 		{
-			String workingDirectory = System.getProperty("user.dir");
-
-			Path nodePath = Paths.get(workingDirectory, "nodes.csv");
-			String nodeFilename = URLEncoder.encode(nodePath.toAbsolutePath()
-					.toString());
-
-			Path edgePath = Paths.get(workingDirectory, "edges.csv");
-			String edgeFilename = URLEncoder.encode(edgePath.toAbsolutePath()
-					.toString());
-
-			String dbName = URLEncoder.encode(cmdLine.getDbName());
-
-			String urlStr = String.format(
-					"http://localhost:2480/importcsv/%s/%s/%s/", nodeFilename,
-					edgeFilename, dbName);
-
-			URL url = new URL(urlStr);
-			HttpURLConnection connection;
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("GET");
 			connection.setReadTimeout(0);
 			connection.getInputStream();
 		}
-		catch (IOException e)
+		catch (ConnectException ex)
 		{
-			e.printStackTrace();
+			System.out
+					.println("You need to start the bjoern-server (bjoern-server.sh).");
+		}
+		catch (IOException ex)
+		{
+			if (ex.getMessage().contains("405"))
+				System.err
+						.println("Cannot invoke server plugin."
+								+ " Try copying `conf/orientdb-server-config.xml "
+								+ "to orientdb-2.1.5-community/config/ and restart the server.`");
 		}
 
 	}
