b7b3d67058d19eb6e9dada4e73b30d6c486e65b9
Alwin Maier <amaier@gwdg.de>
2015-12-16 17:42:35 +0100

    Improve performance.
    Improve performance of control flow graph constructions. The index
    query is limited to basic blocks. Instructions are obtained by
    traversing corresponding edges starting at basic blocks.

--- src/server/components/graphs/CFGCreator.java
+++ src/server/components/graphs/CFGCreator.java
@@ -9,10 +9,11 @@ import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Graph;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphNoTx;
+import com.tinkerpop.blueprints.impls.tg.TinkerGraph;
 
 import server.Constants;
 
-public class CFGCreator extends GraphCreator
+public class CFGCreator
 {
 
 	protected OrientGraphNoTx g;
@@ -24,35 +25,60 @@ public class CFGCreator extends GraphCreator
 
 	public Graph createCFG(Long functionId)
 	{
-		Iterable<Vertex> vertices = getVerticesOfFunction(functionId);
-
-		Iterable<Edge> edges = getInducedEdges(vertices,
-				vertex -> vertex.getEdges(Direction.OUT),
-				edge -> (edge.getLabel().equals("CFLOW_ALWAYS")
-						|| edge.getLabel().equals("CFLOW_TRUE")
-						|| edge.getLabel().equals("CFLOW_FALSE")
-						|| edge.getLabel().equals("IS_BB_OF"))
-						&& edge.getVertex(Direction.IN)
-								.getProperty("functionId")
-								.equals(functionId.toString()));
-
-		return createGraph(vertices, edges);
+		Graph sg = new TinkerGraph();
+		Iterable<Vertex> basicBlocks = getBasicBlocksOfFunction(functionId);
+		// Connect basic blocks with instructions
+		for (Vertex bb : basicBlocks)
+		{
+			Vertex v = sg.addVertex(bb.getId());
+			for (String property : bb.getPropertyKeys())
+			{
+				v.setProperty(property, bb.getProperty(property));
+			}
+			for (Edge edge : bb.getEdges(Direction.OUT, "IS_BB_OF"))
+			{
+				Vertex instr = edge.getVertex(Direction.IN);
+				Vertex w = sg.addVertex(instr);
+				for (String property : instr.getPropertyKeys())
+				{
+					w.setProperty(property, instr.getProperty(property));
+				}
+				Edge e = sg.addEdge(edge.getId(), v, w, edge.getLabel());
+				for (String property : edge.getPropertyKeys())
+				{
+					e.setProperty(property, edge.getProperty(property));
+				}
+			}
+		}
+		// Connect basic blocks with each other
+		for (Vertex bb : basicBlocks)
+		{
+			for (Edge edge : bb.getEdges(Direction.OUT, "CFLOW_ALWAYS",
+					"CFLOW_TRUE", "CFLOW_FALSE"))
+			{
+				Vertex v = sg.getVertex(bb.getId());
+				Vertex w = sg.getVertex(edge.getVertex(Direction.IN));
+				if (w != null)
+				{
+					Edge e = sg.addEdge(edge.getId(), v, w, edge.getLabel());
+					for (String property : edge.getPropertyKeys())
+					{
+						e.setProperty(property, edge.getProperty(property));
+					}
+				}
+			}
+		}
+		return sg;
 	}
 
-	protected Iterable<Vertex> getVerticesOfFunction(Long functionId)
+	protected Iterable<Vertex> getBasicBlocksOfFunction(Long functionId)
 	{
-		String fmt = "SELECT * FROM V WHERE %s LUCENE \"%s\"";
-		String luceneQuery = "functionId:" + functionId;
+		String fmt = "SELECT * FROM V WHERE %s LUCENE \"(%s)\"";
+		String luceneQuery = "functionId:" + functionId + " AND  nodeType:BB";
 		String queryStr = String.format(fmt, Constants.INDEX_NAME, luceneQuery);
 
 		OCommandSQL query = new OCommandSQL(queryStr);
-		List<Vertex> vertices = new LinkedList<Vertex>();
 		Iterable<Vertex> result = g.command(query).execute();
-		for (Vertex obj : result)
-		{
-			vertices.add(obj);
-		}
-		return vertices;
+		return result;
 	}
-
 }
