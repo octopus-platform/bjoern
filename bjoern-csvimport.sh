79c0aac6a0025ee527b83c1ee74852e29dc3d143
Alwin Maier <amaier@gwdg.de>
2016-01-12 17:46:26 +0100

    Update scripts.

---
+++ bjoern-csvimport.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp ./bin/bjoern.jar clients.bjoernImport.BjoernImport $@
--- bjoern-import.sh
+++ bjoern-import.sh
@@ -1,2 +1,22 @@
 #!/bin/sh
-java -cp ./bin/bjoern.jar clients.bjoernImport.BjoernImport $@
+#
+# Arguments:
+# $1 - binary (passed to RadareExporter
+# $2 - database name (optional, passed to BjoernImport)
+#
+# Note: All duplicate rows are removed before import.
+
+TMP=$(mktemp -d)
+trap "rm -rf $TMP" EXIT
+
+./bjoern-radare.sh $1 -outdir $TMP || exit 1
+
+cat $TMP/nodes.csv | sort -r | uniq > nodes.csv
+cat $TMP/edges.csv | sort -r | uniq > edges.csv
+
+if [ -z $2 ]
+then
+	./bjoern-csvimport.sh
+else
+	./bjoern-csvimport.sh -dbname $2
+fi
