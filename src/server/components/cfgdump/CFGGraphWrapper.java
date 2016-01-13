e92c39beca5ed1799fb229fea860fc52bb5d4d84
Alwin Maier <amaier@gwdg.de>
2016-01-13 10:03:12 +0100

    Check if a vertex is already contained.
    Before adding a vertex to the control flow graph, check if it is already
    contained.

--- src/server/components/cfgdump/CFGGraphWrapper.java
+++ src/server/components/cfgdump/CFGGraphWrapper.java
@@ -1,6 +1,7 @@
 package server.components.cfgdump;
 
 import com.tinkerpop.blueprints.Graph;
+import com.tinkerpop.blueprints.Vertex;
 
 public class CFGGraphWrapper extends GraphWrapper
 {
@@ -8,6 +9,17 @@ public class CFGGraphWrapper extends GraphWrapper
 	public CFGGraphWrapper(Graph graph)
 	{
 		super(graph);
+	}
+
+	@Override
+	public void addVertex(Vertex vertex)
+	{
+		//
+		if (contains(vertex))
+		{
+			return;
+		}
+		super.addVertex(vertex);
 	}
 
 }
