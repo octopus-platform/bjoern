45507804434d4d3937f0e085117e6d598104bacd
Fabian Yamaguchi <fyamagu@gwdg.de>
2016-04-19 13:57:02 +0200

    Boilerplate for alocs plugin.

---
+++ projects/bjoern-plugins/alocs/src/main/java/bjoern/plugins/alocs/AlocPlugin.java
@@ -0,0 +1,14 @@
+package bjoern.plugins.alocs;
+
+import bjoern.pluginlib.OrientGraphConnectionPlugin;
+
+public class AlocPlugin extends OrientGraphConnectionPlugin{
+
+	@Override
+	public void execute() throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
