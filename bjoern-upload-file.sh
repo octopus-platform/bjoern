e55035e261eea7c6a4c54e817716285184538d8b
Fabian Yamaguchi <fyamagu@gwdg.de>
2016-04-22 12:00:58 +0200

    Added bjoern-upload-file.sh

---
+++ bjoern-upload-file.sh
@@ -0,0 +1,1 @@
+base64 $1 | curl --header 'Content-Type:us/ascii' -d @- http://localhost:2480/uploadfile/
--- projects/octopus/src/main/java/octopus/server/commands/uploadfile/UploadHandler.java
+++ projects/octopus/src/main/java/octopus/server/commands/uploadfile/UploadHandler.java
@@ -1,12 +1,14 @@
 package octopus.server.commands.uploadfile;
 
 import java.io.FileOutputStream;
+import java.util.Base64;
 
 import com.orientechnologies.orient.server.config.OServerCommandConfiguration;
 import com.orientechnologies.orient.server.network.protocol.http.OHttpRequest;
 import com.orientechnologies.orient.server.network.protocol.http.OHttpResponse;
 import com.orientechnologies.orient.server.network.protocol.http.OHttpUtils;
 import com.orientechnologies.orient.server.network.protocol.http.command.OServerCommandAbstract;
+
 
 public class UploadHandler extends OServerCommandAbstract {
 
@@ -18,8 +20,10 @@ public class UploadHandler extends OServerCommandAbstract {
 	public boolean execute(OHttpRequest iRequest, OHttpResponse iResponse) throws Exception
 	{
 
+		byte[] decoded = Base64.getMimeDecoder().decode(iRequest.content);
+
 		FileOutputStream file = new FileOutputStream("/tmp/lala");
-		file.write(iRequest.content.getBytes());
+		file.write(decoded);
 		file.close();
 
 		iResponse.send(OHttpUtils.STATUS_OK_CODE, "OK", null,
