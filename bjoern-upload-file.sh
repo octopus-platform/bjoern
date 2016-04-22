c1374658610add27048651bacf5133a78a5760a6
Fabian Yamaguchi <fyamagu@gwdg.de>
2016-04-22 12:23:38 +0200

    Upload plugin complete.

--- bjoern-upload-file.sh
+++ bjoern-upload-file.sh
@@ -1,1 +1,5 @@
-base64 $1 | curl --header 'Content-Type:us/ascii' -d @- http://localhost:2480/uploadfile/
+#!/bin/bash
+
+
+
+base64 $1 | curl --header 'Content-Type:us/ascii' -d @- http://localhost:2480/uploadfile/`python -c "import urllib, sys; print urllib.quote(sys.argv[1], safe='')" $2`
--- projects/octopus/src/main/java/octopus/server/commands/uploadfile/UploadHandler.java
+++ projects/octopus/src/main/java/octopus/server/commands/uploadfile/UploadHandler.java
@@ -1,6 +1,8 @@
 package octopus.server.commands.uploadfile;
 
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Base64;
 
 import com.orientechnologies.orient.server.config.OServerCommandConfiguration;
@@ -20,21 +22,36 @@ public class UploadHandler extends OServerCommandAbstract {
 	public boolean execute(OHttpRequest iRequest, OHttpResponse iResponse) throws Exception
 	{
 
+		String dstFilename = extractDstFilenameFromRequest(iRequest);
+
 		byte[] decoded = Base64.getMimeDecoder().decode(iRequest.content);
 
-		FileOutputStream file = new FileOutputStream("/tmp/lala");
-		file.write(decoded);
-		file.close();
+		writeDataToFile(dstFilename, decoded);
 
 		iResponse.send(OHttpUtils.STATUS_OK_CODE, "OK", null,
 				"", null);
 		return false;
 	}
 
+	private void writeDataToFile(String dstFilename, byte[] data) throws FileNotFoundException, IOException {
+		FileOutputStream file = new FileOutputStream(dstFilename);
+		file.write(data);
+		file.close();
+	}
+
+	private String extractDstFilenameFromRequest(OHttpRequest iRequest)
+	{
+		String[] urlParts = checkSyntax(
+				iRequest.url,
+				2,
+				"Syntax error: importcsv/<dstFilename>");
+		return urlParts[1];
+	}
+
 	@Override
 	public String[] getNames()
 	{
-		return new String[] {"POST|uploadfile/"};
+		return new String[] {"POST|uploadfile/*"};
 	}
 
 }
