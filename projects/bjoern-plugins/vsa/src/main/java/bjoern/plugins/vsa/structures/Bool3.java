5a50f79a1b92c27812f9a06e51f9642290bef35d
Alwin Maier <alwin.maier@tu-bs.de>
2016-04-28 16:08:53 +0200

    Add Bool3 class.

---
+++ projects/bjoern-plugins/vsa/src/main/java/bjoern/plugins/vsa/structures/Bool3.java
@@ -0,0 +1,102 @@
+package bjoern.plugins.vsa.structures;
+
+public enum Bool3
+{
+	TRUE,
+	FALSE,
+	MAYBE;
+
+	public Bool3 and(Bool3 bool)
+	{
+		if (this == TRUE)
+		{
+			return bool;
+		} else if (this == FALSE)
+		{
+			return FALSE;
+		} else
+		{
+			if (bool == FALSE)
+			{
+				return MAYBE
+			} else
+			{
+				return bool;
+			}
+		}
+	}
+
+	public Bool3 or(Bool3 bool)
+	{
+		if (this == TRUE)
+		{
+			return TRUE;
+		} else if (this == FALSE)
+		{
+			return bool;
+		} else
+		{
+			if (bool == FALSE)
+			{
+				return MAYBE;
+			} else
+			{
+				return bool;
+			}
+		}
+	}
+
+	public Bool3 not()
+	{
+		if (this == TRUE)
+		{
+			return FALSE;
+		} else if (this == FALSE)
+		{
+			return TRUE;
+		} else
+		{
+			return MAYBE;
+		}
+	}
+
+	public Bool3 xor(Bool3 bool)
+	{
+		if (this == TRUE)
+		{
+			return bool.not();
+		} else if (this == FALSE)
+		{
+			return bool;
+		} else
+		{
+			return MAYBE;
+		}
+	}
+
+	public Bool3 join(Bool3 bool)
+	{
+		if (this == TRUE)
+		{
+			if (bool == FALSE)
+			{
+				return MAYBE;
+			} else
+			{
+				return bool;
+			}
+		} else if (this == FALSE)
+		{
+			if (bool == TRUE)
+			{
+				return MAYBE;
+			} else
+			{
+				return bool;
+			}
+		} else
+		{
+			return MAYBE;
+		}
+	}
+}
