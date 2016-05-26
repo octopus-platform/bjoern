0b23d9db735519548e11e4eaeb3da769e6ed0437
Alwin Maier <alwin.maier@tu-bs.de>
2016-05-26 14:19:07 +0200

    Add new traversals.

--- projects/bjoern-plugins/pluginlib/src/main/java/bjoern/pluginlib/Traversals.java
+++ projects/bjoern-plugins/pluginlib/src/main/java/bjoern/pluginlib/Traversals.java
@@ -1,31 +1,29 @@
 package bjoern.pluginlib;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
-import com.tinkerpop.blueprints.Vertex;
-import com.tinkerpop.gremlin.java.GremlinPipeline;
-import com.tinkerpop.pipes.PipeFunction;
-
 import bjoern.nodeStore.NodeTypes;
 import bjoern.pluginlib.structures.BasicBlock;
+import bjoern.pluginlib.structures.Function;
 import bjoern.pluginlib.structures.Instruction;
 import bjoern.structures.BjoernNodeProperties;
 import bjoern.structures.edges.EdgeTypes;
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.gremlin.java.GremlinPipeline;
 
-public class Traversals {
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+public class Traversals
+{
 
 	public static BasicBlock functionToEntryBlock(Vertex func)
 	{
 		GremlinPipeline<Vertex, Vertex> pipe = createNewGremlinPipe();
 
-		pipe.start(func).in(EdgeTypes.INTERPRETATION)
-		.out(EdgeTypes.INTERPRETATION).filter(new PipeFunction<Vertex,Boolean>() {
-				@Override
-				public Boolean compute(Vertex v) {
-					return v.getProperty(BjoernNodeProperties.TYPE).equals(NodeTypes.BASIC_BLOCK);
-				  }
-			}
+		pipe.start(func).in(EdgeTypes.INTERPRETATION).out(EdgeTypes.INTERPRETATION).filter(
+				v -> v.getProperty(BjoernNodeProperties.TYPE).equals
+						(NodeTypes.BASIC_BLOCK)
 		);
 
 		Vertex vertex = getFirstVertexFromPipeOrRaise(pipe);
@@ -40,25 +38,51 @@ public class Traversals {
 
 		List<Vertex> list = pipe.toList();
 
-		return list.stream().map(Instruction :: new).collect(Collectors.toList());
+		return list.stream().map(Instruction::new).collect(Collectors.toList());
 	}
+
 
 	private static GremlinPipeline<Vertex, Vertex> createNewGremlinPipe()
 	{
-		return new GremlinPipeline<Vertex,Vertex>();
+		return new GremlinPipeline<>();
 	}
 
 	private static Vertex getFirstVertexFromPipeOrRaise(GremlinPipeline<Vertex, Vertex> pipe)
 	{
-		if(!pipe.hasNext())
+		if (!pipe.hasNext())
 			throw new RuntimeException("Empty pipeline");
 
 		Vertex vertex = pipe.next();
 
-		if(vertex == null)
+		if (vertex == null)
 			throw new RuntimeException("Empty pipeline");
 
 		return vertex;
+	}
+
+	public static Instruction functionToEntryInstruction(Function function)
+	{
+		GremlinPipeline<Vertex, Vertex> pipeline = createNewGremlinPipe();
+
+		Vertex vertex;
+		pipeline.start(function.getNode()).in(EdgeTypes.INTERPRETATION).out(EdgeTypes.INTERPRETATION)
+				.filter(v -> v.getProperty(BjoernNodeProperties.TYPE).equals(NodeTypes.INSTRUCTION));
+
+		if (pipeline.hasNext())
+		{
+			return new Instruction(pipeline.next());
+		} else
+		{
+			return null;
+		}
+	}
+
+	public static List<Instruction> instructionToSuccessors(Instruction instruction)
+	{
+		return StreamSupport.stream(instruction.getNode()
+				.getVertices(Direction.OUT, GraphOperations.INSTR_CFLOW_EDGE)
+				.spliterator(), false)
+				.map(Instruction::new).collect(Collectors.toList());
 	}
 
 }
