4f39256c125292b0c086022b6f0b9814f0e85a26
Alwin Maier <alwin.maier@tu-bs.de>
2016-05-30 15:56:09 +0200

    Simplify DotWriter.

--- projects/bjoern-plugins/functionexport/src/main/java/bjoern/plugins/functionexporter/io/dot/DotWriter.java
+++ projects/bjoern-plugins/functionexport/src/main/java/bjoern/plugins/functionexporter/io/dot/DotWriter.java
@@ -14,16 +14,6 @@ public class DotWriter
 
 	private Graph graph;
 	private Writer writer;
-	private static final Map<String, String> nodeAttributes;
-	private static final Map<String, String> edgeAttributes;
-
-	static
-	{
-		nodeAttributes = new HashMap<>();
-		edgeAttributes = new HashMap<>();
-		nodeAttributes.put("fontsize", "10");
-		edgeAttributes.put("fontsize", "8");
-	}
 
 	/**
 	 * @param graph the Graph to pull the data from
@@ -43,9 +33,6 @@ public class DotWriter
 		writer.write(" {");
 		writer.write(DotTokens.NEWLINE);
 
-		writeNodeAttributes();
-		writeEdgeAttributes();
-
 		for (Vertex vertex : graph.getVertices())
 		{
 			writeVertex(vertex);
@@ -61,30 +48,6 @@ public class DotWriter
 		writer.flush();
 	}
 
-	private void writeNodeAttributes() throws IOException
-	{
-		writer.write(DotTokens.NODE);
-		writer.write(" [ ");
-		for (String key : nodeAttributes.keySet())
-		{
-			writer.write(key + "=\"" + nodeAttributes.get(key) + "\" ");
-		}
-		writer.write("];");
-		writer.write(DotTokens.NEWLINE);
-	}
-
-	private void writeEdgeAttributes() throws IOException
-	{
-		writer.write(DotTokens.EDGE);
-		writer.write(" [ ");
-		for (String key : edgeAttributes.keySet())
-		{
-			writer.write(key + "=\"" + edgeAttributes.get(key) + "\" ");
-		}
-		writer.write("];");
-		writer.write(DotTokens.NEWLINE);
-	}
-
 	private void writeEdge(Edge edge) throws IOException
 	{
 		Vertex tail = edge.getVertex(Direction.OUT);
@@ -92,7 +55,8 @@ public class DotWriter
 		writer.write(id(tail));
 		writer.write(DotTokens.EDGE_OP);
 		writer.write(id(head));
-		writer.write(" [ label=\"" + label(edge) + "\" ");
+		writer.write(" [ ");
+		writer.write("label=\"" + edge.getLabel() + "\" ");
 		for (String key : edge.getPropertyKeys())
 		{
 			String value = edge.getProperty(key).toString();
@@ -107,7 +71,6 @@ public class DotWriter
 	{
 		writer.write(id(vertex));
 		writer.write(" [ ");
-		writer.write("label=\"" + label(vertex) + "\" ");
 		for (String key : vertex.getPropertyKeys())
 		{
 			String value = vertex.getProperty(key).toString();
@@ -124,23 +87,13 @@ public class DotWriter
 		return vertex.getId().toString().split(":")[1];
 	}
 
-	private String label(Vertex vertex)
-	{
-		return vertex.getProperty("dotlabel");
-	}
-
-	private String label(Edge edge)
-	{
-		return edge.getLabel();
-	}
-
 	private String escape(String string)
 	{
 		return string.replace("\"", "\\\"");
 	}
 
 	public static void outputGraph(final Graph graph,
-			final OutputStream dotOutputStream) throws IOException
+								   final OutputStream dotOutputStream) throws IOException
 	{
 
 		DotWriter writer = new DotWriter(graph);
