79e6cc8828f7fddb5b21217211f757d49819aa75
Alwin Maier <alwin.maier@tu-bs.de>
2016-06-21 09:33:04 +0200

    Update node structures.

--- projects/bjoern-pluginlib/src/main/java/bjoern/pluginlib/structures/Instruction.java
+++ projects/bjoern-pluginlib/src/main/java/bjoern/pluginlib/structures/Instruction.java
@@ -1,10 +1,11 @@
 package bjoern.pluginlib.structures;
 
 import bjoern.nodeStore.NodeTypes;
+import bjoern.pluginlib.Traversals;
 import bjoern.structures.BjoernNodeProperties;
 import bjoern.structures.edges.EdgeTypes;
-import com.tinkerpop.blueprints.Direction;
 import com.tinkerpop.blueprints.Vertex;
+import com.tinkerpop.gremlin.java.GremlinPipeline;
 import octopus.lib.structures.OctopusNode;
 
 public class Instruction extends OctopusNode implements Comparable<Instruction>
@@ -47,6 +48,24 @@ public class Instruction extends OctopusNode implements Comparable<Instruction>
 
 	public boolean isCall()
 	{
-		return getEdges(Direction.OUT, EdgeTypes.CALL).iterator().hasNext();
+		return call().hasNext();
+	}
+
+	public GremlinPipeline<?, Instruction> call()
+	{
+		return new GremlinPipeline<>(this.getBaseVertex()).out(EdgeTypes.CALL).transform(Instruction::new);
+	}
+
+	public GremlinPipeline<?, Instruction> exits()
+	{
+		final int maxLoops = 10000;
+		return new GremlinPipeline<>(this.getBaseVertex()).as("start")
+				.out(Traversals.INSTR_CFLOW_EDGE, Traversals.INSTR_CFLOW_TRANSITIVE_EDGE).dedup().loop("start",
+						arg -> arg.getLoops() < maxLoops
+								&& !arg.getObject().getProperty(BjoernNodeProperties.REPR).equals("ret")
+								&& !arg.getObject().getProperty(BjoernNodeProperties.REPR).equals("jmp section..plt"),
+						arg -> arg.getObject().getProperty(BjoernNodeProperties.REPR).equals("ret")
+								|| arg.getObject().getProperty(BjoernNodeProperties.REPR).equals("jmp section..plt"))
+				.transform(Instruction::new);
 	}
 }
