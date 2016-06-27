bbe581ba7a26866f4ec4e5bf7e4ea3194d0d9105
Fabian Yamaguchi <fyamagu@gwdg.de>
2016-06-27 16:27:42 +0200

    Moved queryNodeIndex from octopus-lang to bjoern-lang.

--- projects/bjoern-lang/src/main/groovy/lookup.groovy
+++ projects/bjoern-lang/src/main/groovy/lookup.groovy
@@ -1,5 +1,12 @@
 package bjoernsteps;
 
+queryNodeIndex = { luceneQuery ->
+
+	queryStr = 'SELECT * FROM V WHERE [addr,childNum,code,comment,esil,key,nodeType,repr] LUCENE "' + luceneQuery + '"';
+	query = new com.orientechnologies.orient.core.sql.OCommandSQL(queryStr);
+	g.getRawGraph().command(query).execute().toList()._().transform{ g.v(it.getIdentity()) }
+}
+
 getCallsTo = { callee ->
  query = String.format('nodeType:Instr AND repr:call*')
  queryNodeIndex(query).filter{ it.repr.contains(callee) }
