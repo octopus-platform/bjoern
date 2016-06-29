6b51edbf9321a5f4f9837c9e3a2bdae8d884e847
Alwin Maier <alwin.maier@tu-bs.de>
2016-06-29 16:34:12 +0200

    Move 'uploadfile' command.
    File upload is now part of the 'manageprojects' command.

--- projects/octopus/octopus-server/src/main/java/octopus/server/commands/manageprojects/ManageProjectsHandler.java
+++ projects/octopus/octopus-server/src/main/java/octopus/server/commands/manageprojects/ManageProjectsHandler.java
@@ -8,9 +8,13 @@ import com.orientechnologies.orient.server.network.protocol.http.OHttpUtils;
 import com.orientechnologies.orient.server.network.protocol.http.command.OServerCommandAbstract;
 import octopus.server.components.projectmanager.ProjectManager;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Base64;
 
 public class ManageProjectsHandler extends OServerCommandAbstract
 {
@@ -60,12 +64,43 @@ public class ManageProjectsHandler extends OServerCommandAbstract
 		} else if (command.equals("list"))
 		{
 			return executeList(iRequest, iResponse);
+		} else if (command.equals("uploadfile"))
+		{
+			return executeUpload(iRequest, iResponse);
+
 		} else
 		{
 			iResponse.send(OHttpUtils.STATUS_NOTFOUND_CODE, "Not found", null, "", null);
 			return false;
 		}
 
+	}
+
+	private boolean executeUpload(OHttpRequest iRequest, OHttpResponse iResponse) throws IOException
+	{
+		String[] urlParts = checkSyntax(iRequest.url, 4,
+				"Syntax error: manageprojects/uploadfile/projectName/relativePath");
+
+		String projectName = urlParts[2];
+		String relativePath = urlParts[3];
+
+		String pathToProject = ProjectManager.getPathToProject(projectName);
+		Path dstFilename = Paths.get(pathToProject, relativePath);
+		byte[] decodedData = Base64.getMimeDecoder().decode(iRequest.content);
+
+		writeDataToFile(dstFilename, decodedData);
+		iResponse.send(OHttpUtils.STATUS_OK_CODE, "OK", null, "", null);
+
+		return false;
+
+	}
+
+	private void writeDataToFile(Path path, byte[] data) throws IOException
+	{
+		try (OutputStream out = Files.newOutputStream(path))
+		{
+			out.write(data);
+		}
 	}
 
 	private boolean executeCreate(OHttpRequest iRequest, OHttpResponse iResponse) throws Exception
@@ -100,7 +135,7 @@ public class ManageProjectsHandler extends OServerCommandAbstract
 	@Override
 	public String[] getNames()
 	{
-		return new String[]{"GET|manageprojects/*"};
+		return new String[]{"GET|manageprojects/*", "POST|manageprojects/uploadfile/*"};
 	}
 
 }
