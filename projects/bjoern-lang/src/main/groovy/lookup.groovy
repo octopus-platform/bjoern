b7e7834ace3dd7b23e2f41174852134c214d75c9
Fabian Yamaguchi <fyamagu@gwdg.de>
2016-07-15 14:05:30 +0200

    Fix lookups.

--- projects/bjoern-lang/src/main/groovy/lookup.groovy
+++ projects/bjoern-lang/src/main/groovy/lookup.groovy
@@ -2,7 +2,7 @@ package bjoernsteps;
 
 queryNodeIndex = { luceneQuery ->
 
-    queryStr = 'SELECT * FROM V WHERE [addr,code,comment,esil,key,nodeType,repr] LUCENE "' + luceneQuery + '"';
+    queryStr = 'SELECT * FROM V WHERE [addr,childNum,code,comment,esil,key,nodeType,repr] LUCENE "' + luceneQuery + '"';
     query = new com.orientechnologies.orient.core.sql.OCommandSQL(queryStr);
     g.getRawGraph().command(query).execute().toList()._().transform { g.v(it.getIdentity()) }
 }
