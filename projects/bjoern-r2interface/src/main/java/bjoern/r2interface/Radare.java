1b31df08145f1fcc0c76e3e05ad140e2e6189b8b
Alwin Maier <alwin.maier@tu-bs.de>
2016-09-28 17:33:29 +0200

    Get local variables from radare.

--- projects/bjoern-r2interface/src/main/java/bjoern/r2interface/Radare.java
+++ projects/bjoern-r2interface/src/main/java/bjoern/r2interface/Radare.java
@@ -15,24 +15,22 @@ import java.io.IOException;
 import java.io.StringReader;
 import java.util.*;
 
-public class Radare
-{
+public class Radare {
 	private R2Pipe r2Pipe;
 
 	private HashMap<String, Integer> regWidthHash;
 	private HashMap<String, RegisterFamily> regFamilyHash;
 
-	private static final Logger logger = LoggerFactory.getLogger(Radare.class);
+	private static final Logger logger = LoggerFactory.getLogger(
+			Radare.class);
 
-	public void loadBinary(String filename) throws IOException
-	{
+	public void loadBinary(String filename) throws IOException {
 		r2Pipe = new R2Pipe(filename);
 		setRadareOptions();
 		initializeRegisterHashs();
 	}
 
-	private void initializeRegisterHashs() throws IOException
-	{
+	private void initializeRegisterHashs() throws IOException {
 		String arpResult = r2Pipe.cmd("arp");
 		String line;
 
@@ -41,10 +39,8 @@ public class Radare
 		BufferedReader buffer = new BufferedReader(new StringReader
 				(arpResult));
 
-		while (null != (line = buffer.readLine()))
-		{
-			if (!line.startsWith("gpr"))
-			{
+		while (null != (line = buffer.readLine())) {
+			if (!line.startsWith("gpr")) {
 				continue;
 			}
 
@@ -58,22 +54,19 @@ public class Radare
 		}
 	}
 
-	private void updateRegWidthHash(String register, int widthInBit)
-	{
+	private void updateRegWidthHash(String register, int widthInBit) {
 		regWidthHash.put(register, widthInBit);
 	}
 
-	private void updateRegFamilyHash(String register, int widthInBit,
-			int globalStart)
-	{
+	private void updateRegFamilyHash(
+			String register, int widthInBit,
+			int globalStart) {
 		RegisterFamily newFamily = new RegisterFamily(register, globalStart,
 				globalStart + widthInBit / 8 - 1);
 
 		for (Map.Entry<String, RegisterFamily> entry : regFamilyHash
-				.entrySet())
-		{
-			if (entry.getValue().overlaps(newFamily))
-			{
+				.entrySet()) {
+			if (entry.getValue().overlaps(newFamily)) {
 				newFamily.merge(entry.getValue());
 				entry.setValue(newFamily);
 			}
@@ -81,42 +74,39 @@ public class Radare
 		regFamilyHash.put(register, newFamily);
 	}
 
-	public void analyzeBinary() throws IOException
-	{
+	public void analyzeBinary() throws IOException {
 		r2Pipe.cmd("aaa");
 	}
 
-	private void setRadareVariable(String group, String variable,
-			String value) throws IOException
-	{
+	private void setRadareVariable(
+			String group, String variable,
+			String value) throws IOException {
 		r2Pipe.cmd("e " + group + "." + variable + " = " + value);
 	}
 
-	private String getRadareVariable(String group,
-			String variable) throws IOException
-	{
+	private String getRadareVariable(
+			String group,
+			String variable) throws IOException {
 		return r2Pipe.cmd("e " + group + " " + variable);
 	}
 
-	private void setASMVariable(String variable,
-			String value) throws IOException
-	{
+	private void setASMVariable(
+			String variable,
+			String value) throws IOException {
 		setRadareVariable("asm", variable, value);
 	}
 
-	private String getASMVariable(String variable) throws IOException
-	{
+	private String getASMVariable(String variable) throws IOException {
 		return getRadareVariable("asm", variable);
 	}
 
-	private void setSCRVariable(String variable,
-			String value) throws IOException
-	{
+	private void setSCRVariable(
+			String variable,
+			String value) throws IOException {
 		setRadareVariable("scr", variable, value);
 	}
 
-	private void setRadareOptions() throws IOException
-	{
+	private void setRadareOptions() throws IOException {
 		setSCRVariable("color", "false");
 		setASMVariable("bytes", "false");
 		setASMVariable("lines", "false");
@@ -127,13 +117,11 @@ public class Radare
 		setASMVariable("esil", "true");
 	}
 
-	public void saveProject(String projectFilename) throws IOException
-	{
+	public void saveProject(String projectFilename) throws IOException {
 		r2Pipe.cmd("Ps " + projectFilename);
 	}
 
-	public void loadProject(String projectFilename) throws IOException
-	{
+	public void loadProject(String projectFilename) throws IOException {
 		r2Pipe.cmd("Po " + projectFilename);
 		// This is a workaround: for some reason, when loading a
 		// project, r2 goes out of quiet mode
@@ -142,49 +130,43 @@ public class Radare
 		setSCRVariable("color", "false");
 	}
 
-	public Architecture getArchitecture() throws IOException
-	{
+	public Architecture getArchitecture() throws IOException {
 		String arch = getASMVariable("arch");
 		String bits = getASMVariable("bits");
 
-		// TODO: Actually take a look at `arch` and `bits` to decide which architecture to return
+		// TODO: Actually take a look at `arch` and `bits` to decide which
+		// architecture to return
 		return new X64Architecture();
 	}
 
-	public JSONArray getJSONFunctions() throws IOException
-	{
+	public JSONArray getJSONFunctions() throws IOException {
 		String str = r2Pipe.cmd("aflj");
-		try
-		{
+		try {
 			return new JSONArray(str);
-		} catch (JSONException e)
-		{
+		} catch (JSONException e) {
 			return null;
 		}
 	}
 
 	public JSONObject getJSONFunctionContentAt(Long addr)
-			throws InvalidRadareFunctionException, IOException
-	{
+			throws InvalidRadareFunctionException, IOException {
 		String jsonStr = r2Pipe.cmd("agj 0x" + Long.toHexString(addr));
 
 		JSONArray jsonArray;
-		try
-		{
+		try {
 			jsonArray = new JSONArray(jsonStr);
-		} catch (JSONException ex)
-		{
+		} catch (JSONException ex) {
 			return null;
 		}
 
-		if (jsonArray.length() != 1)
+		if (jsonArray.length() != 1) {
 			throw new InvalidRadareFunctionException(
 					"empty function at address 0x" + Long.toHexString(addr));
+		}
 
 		JSONObject functionJSONObject = jsonArray.getJSONObject(0);
 		Long addressOfReceivedFunction = functionJSONObject.getLong("offset");
-		if (!addr.equals(addressOfReceivedFunction))
-		{
+		if (!addr.equals(addressOfReceivedFunction)) {
 			throw new InvalidRadareFunctionException(
 					"requested function content for address 0x"
 							+ Long.toHexString(addr)
@@ -194,43 +176,35 @@ public class Radare
 		return functionJSONObject;
 	}
 
-	public void shutdown() throws Exception
-	{
+	public void shutdown() throws Exception {
 		r2Pipe.quit();
 	}
 
-	public JSONArray getFlags() throws IOException
-	{
+	public JSONArray getFlags() throws IOException {
 		String jsonString = r2Pipe.cmd("fj");
-		try
-		{
+		try {
 			return new JSONArray(jsonString);
-		} catch (JSONException e)
-		{
+		} catch (JSONException e) {
 			return null;
 		}
 	}
 
-	public JSONArray getReferences() throws IOException
-	{
+	public JSONArray getReferences() throws IOException {
 		JSONArray answer = new JSONArray();
 		String rawString = r2Pipe.cmd("ax");
 		Scanner scanner = new Scanner(rawString);
 		// Skip first line (header)
-		if (scanner.hasNextLine())
-		{
+		if (scanner.hasNextLine()) {
 			scanner.nextLine();
 		}
-		while (scanner.hasNext())
-		{
+		while (scanner.hasNext()) {
 			JSONObject jsonReference = parseReferenceLine(scanner.nextLine());
 			answer.put(jsonReference);
 		}
 		return answer;
 	}
 
-	private JSONObject parseReferenceLine(String line)
-	{
+	private JSONObject parseReferenceLine(String line) {
 		// The JSON object does not contain type information. We have to parse
 		// by hand.
 		// line format "type0.type1.type2.source=destination0,destination1,
@@ -242,122 +216,150 @@ public class Radare
 		answer.put("address", Long.decode(addresses[0]));
 		answer.put("locations", new JSONArray(
 				Arrays.stream(addresses[1].split(",")).map(Long::decode)
-						.toArray()));
+				      .toArray()));
 		return answer;
 	}
 
-	public void resetEsilState() throws IOException
-	{
+	public void resetEsilState() throws IOException {
 		r2Pipe.cmd("ar0");
 		r2Pipe.cmd("aei");
 		r2Pipe.cmd("aeim");
 	}
 
-	public String runEsilCode(String esilCode) throws IOException
-	{
+	public String runEsilCode(String esilCode) throws IOException {
 		return r2Pipe.cmd(String.format("\"ae %s\"", esilCode));
 	}
 
-	public String getRegisterValue(String registerStr) throws IOException
-	{
+	public String getRegisterValue(String registerStr) throws IOException {
 		return r2Pipe.cmd(String.format("ar %s", registerStr));
 	}
 
-	public List<String> getRegistersWritten(Long addr) throws IOException
-	{
+	public List<String> getRegistersWritten(Long addr) throws IOException {
 		String cmd = "aeaw @ " + Long.toUnsignedString(addr);
 		return cmdAndSplitResultAtWhitespace(cmd);
 	}
 
-	public List<String> getRegistersRead(Long addr) throws IOException
-	{
+	public List<String> getRegistersRead(Long addr) throws IOException {
 		String cmd = "aear @ " + Long.toUnsignedString(addr);
 		return cmdAndSplitResultAtWhitespace(cmd);
 	}
 
 
 	private List<String> cmdAndSplitResultAtWhitespace(
-			String cmd) throws IOException
-	{
+			String cmd) throws IOException {
 		String registers = r2Pipe.cmd(cmd).trim();
-		if (registers.length() == 0)
+		if (registers.length() == 0) {
 			return new LinkedList<>();
+		}
 
 		String[] registersAr = registers.split(" ");
 		return Arrays.asList(registersAr);
 	}
 
-	public int getRegisterWidth(String register)
-	{
+	public int getRegisterWidth(String register) {
 		return regWidthHash.get(register);
 	}
 
-	public String getRegisterFamily(String register)
-	{
+	public String getRegisterFamily(String register) {
 		return regFamilyHash.get(register).getName();
 	}
 
-	public String getRegisterRole(String register) throws IOException
-	{
+	public String getRegisterRole(String register) throws IOException {
 		JSONArray jsonArray = getRegisterProfile().getJSONArray("alias_info");
-		for (int i = 0; i < jsonArray.length(); i++)
-		{
+		for (int i = 0; i < jsonArray.length(); i++) {
 			JSONObject aliasInfo = jsonArray.getJSONObject(i);
-			if (aliasInfo.getString("reg").equals(register))
-			{
+			if (aliasInfo.getString("reg").equals(register)) {
 				return aliasInfo.getString("role_str");
 			}
 		}
 		return null;
 	}
 
-	public String getRegisterByRole(String role) throws IOException
-	{
+	public String getRegisterByRole(String role) throws IOException {
 		JSONArray jsonArray = getRegisterProfile().getJSONArray("alias_info");
-		for (int i = 0; i < jsonArray.length(); i++)
-		{
+		for (int i = 0; i < jsonArray.length(); i++) {
 			JSONObject aliasInfo = jsonArray.getJSONObject(i);
-			if (aliasInfo.getString("role_str").equals(role))
-			{
+			if (aliasInfo.getString("role_str").equals(role)) {
 				return aliasInfo.getString("reg");
 			}
 		}
 		return null;
 	}
 
-	private JSONObject getRegisterProfile() throws IOException
-	{
+	private JSONObject getRegisterProfile() throws IOException {
 		String jsonString = r2Pipe.cmd("arpj");
 		JSONObject registerProfile = new JSONObject(jsonString);
 		return registerProfile;
 	}
 
-	public boolean isFlag(String registerName) throws IOException
-	{
+	public boolean isFlag(String registerName) throws IOException {
 		JSONArray jsonArray = getRegisterProfile().getJSONArray("reg_info");
-		for (int i = 0; i < jsonArray.length(); i++)
-		{
+		for (int i = 0; i < jsonArray.length(); i++) {
 			JSONObject registerInformation = jsonArray.getJSONObject(i);
-			if (registerInformation.getString("name").equals(registerName))
-			{
-				return registerInformation.getString("type_str").equals("flg");
+			if (registerInformation.getString("name").equals(registerName)) {
+				return registerInformation.getString("type_str")
+				                          .equals("flg");
 			}
 		}
 		throw new IllegalArgumentException();
 	}
 
 	public boolean isGeneralPurposeRegister(
-			String registerName) throws IOException
-	{
+			String registerName) throws IOException {
 		JSONArray jsonArray = getRegisterProfile().getJSONArray("reg_info");
-		for (int i = 0; i < jsonArray.length(); i++)
-		{
+		for (int i = 0; i < jsonArray.length(); i++) {
 			JSONObject registerInformation = jsonArray.getJSONObject(i);
-			if (registerInformation.getString("name").equals(registerName))
-			{
-				return registerInformation.getString("type_str").equals("gpr");
+			if (registerInformation.getString("name").equals(registerName)) {
+				return registerInformation.getString("type_str")
+				                          .equals("gpr");
 			}
 		}
 		throw new IllegalArgumentException();
 	}
+
+	public JSONArray getBasePointerBasedVariables(Long functionAddress) throws
+			IOException {
+		// radare's json string is broken (use of hex numbers);
+		// we need to parse by hand
+		String cmd = "afvb @ 0x" + Long.toHexString(functionAddress);
+		String result = r2Pipe
+				.cmd(cmd);
+		JSONArray answer = new JSONArray();
+		try (BufferedReader reader = new BufferedReader(
+				new StringReader(result))) {
+			for (String row = reader.readLine(); row != null;
+					row = reader.readLine()) {
+				JSONObject variable = parseAVFBRow(row);
+				answer.put(variable);
+			}
+		}
+		return answer;
+	}
+
+	private JSONObject parseAVFBRow(String s) {
+		JSONObject variable = new JSONObject();
+		String[] row = s.split(" ");
+		variable.put("kind", row[0]);
+		variable.put("type", row[1]);
+		variable.put("name", row[2]);
+		JSONObject ref = new JSONObject();
+
+		char sign;
+		if (variable.getString("kind").equals("var")) {
+			sign = '-';
+		} else if (variable.getString("kind").equals("arg")) {
+			sign = '+';
+		} else {
+			return null;
+		}
+		int splitIndex = row[4].indexOf(sign);
+		String base = row[4].substring(0, splitIndex);
+		String offset = row[4].substring(splitIndex);
+
+		ref.put("base", base);
+		ref.put("offset", Integer.decode(offset));
+		variable.put("ref", ref);
+		return variable;
+	}
+
 }
