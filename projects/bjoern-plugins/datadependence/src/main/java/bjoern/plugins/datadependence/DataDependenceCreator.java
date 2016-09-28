0aad7827a20d5cb7373e5108d76816b208b30cb1
Alwin Maier <alwin.maier@tu-bs.de>
2016-09-28 11:48:24 +0200

    Generify reaching definitions algorithm.

--- projects/bjoern-plugins/datadependence/src/main/java/bjoern/plugins/datadependence/DataDependenceCreator.java
+++ projects/bjoern-plugins/datadependence/src/main/java/bjoern/plugins/datadependence/DataDependenceCreator.java
@@ -8,26 +8,21 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class DataDependenceCreator
-{
+public class DataDependenceCreator {
 	private static final String LABEL = "REACHES";
 
 	public static void createFromReachingDefinitions(
 			Map<Vertex, Set<ReachingDefinitionAnalyser.Definition>>
-					reachingDefinitions)
-	{
+					reachingDefinitions) {
 		for (Map.Entry<Vertex, Set<ReachingDefinitionAnalyser.Definition>>
 				entry : reachingDefinitions
-				.entrySet())
-		{
+				.entrySet()) {
 			for (ReachingDefinitionAnalyser.Definition definition : entry
-					.getValue())
-			{
+					.getValue()) {
 				Object object = definition.getIdentifier();
 				Vertex destination = entry.getKey();
 				Set<String> useSet = getUseSet(destination);
-				if (useSet.contains(object))
-				{
+				if (useSet.contains(object)) {
 					Vertex source = definition.getLocation();
 					addEdge(source, destination, object);
 				}
@@ -41,20 +36,21 @@ public class DataDependenceCreator
 	 * <p>
 	 * An edge is only added if it does not exist already.
 	 *
-	 * @param source      the source node of the data dependence, i.e.the node
-	 *                    that changes the value of {@code object}
-	 * @param destination the destination node of the data dependence, ie.e
-	 *                    the node that reads the value of {@code object}
-	 * @param object      the data object.
+	 * @param source
+	 * 		the source node of the data dependence, i.e.the node that changes the
+	 * 		value of {@code object}
+	 * @param destination
+	 * 		the destination node of the data dependence, ie.e the node that reads
+	 * 		the value of {@code object}
+	 * @param object
+	 * 		the data object.
 	 */
-	private static void addEdge(Vertex source, Vertex destination,
-			Object object)
-	{
-		for (Edge edge : source.getEdges(Direction.OUT, LABEL))
-		{
+	private static void addEdge(
+			Vertex source, Vertex destination,
+			Object object) {
+		for (Edge edge : source.getEdges(Direction.OUT, LABEL)) {
 			if (edge.getVertex(Direction.IN).equals(destination) && edge
-					.getProperty("aloc").equals(object))
-			{
+					.getProperty("aloc").equals(object)) {
 				// edge exists -> skip
 				return;
 			}
@@ -63,11 +59,9 @@ public class DataDependenceCreator
 		edge.setProperty("aloc", object);
 	}
 
-	private static Set<String> getUseSet(Vertex destination)
-	{
+	private static Set<String> getUseSet(Vertex destination) {
 		Set<String> set = new HashSet<>();
-		for (Vertex vertex : destination.getVertices(Direction.OUT, "READ"))
-		{
+		for (Vertex vertex : destination.getVertices(Direction.OUT, "READ")) {
 			set.add(vertex.getProperty("name"));
 		}
 		return set;
--- projects/bjoern-plugins/datadependence/src/main/java/bjoern/plugins/datadependence/DataDependencePlugin.java
+++ projects/bjoern-plugins/datadependence/src/main/java/bjoern/plugins/datadependence/DataDependencePlugin.java
@@ -4,26 +4,28 @@ import bjoern.pluginlib.LookupOperations;
 import bjoern.pluginlib.Traversals;
 import bjoern.pluginlib.structures.Function;
 import bjoern.pluginlib.structures.Instruction;
+import com.tinkerpop.blueprints.Direction;
+import com.tinkerpop.blueprints.Edge;
 import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphNoTx;
+import com.tinkerpop.gremlin.java.GremlinPipeline;
 import octopus.lib.plugintypes.OrientGraphConnectionPlugin;
 
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
-public class DataDependencePlugin extends OrientGraphConnectionPlugin
-{
+public class DataDependencePlugin extends OrientGraphConnectionPlugin {
 	@Override
-	public void execute() throws Exception
-	{
+	public void execute() throws Exception {
 		OrientGraphNoTx graph = orientConnector.getNoTxGraphInstance();
-		ReachingDefinitionAnalyser analyser = new ReachingDefinitionAnalyser();
-		for (Function function : LookupOperations.getFunctions(graph))
-		{
+		ReachingDefinitionAnalyser analyser = new ReachingDefinitionAnalyser(
+				DefinitionProvider::generatedDefinitions,
+				DefinitionProvider::killedDefinitions);
+		for (Function function : LookupOperations.getFunctions(graph)) {
 			Instruction entry = Traversals
 					.functionToEntryInstruction(function);
-			if (null == entry)
-			{
+			if (null == entry) {
 				continue;
 			}
 			Map<Vertex, Set<ReachingDefinitionAnalyser.Definition>>
@@ -32,6 +34,51 @@ public class DataDependencePlugin extends OrientGraphConnectionPlugin
 			DataDependenceCreator
 					.createFromReachingDefinitions(reachingDefinitions);
 
+		}
+	}
+
+	private static class DefinitionProvider {
+
+		public static Set<ReachingDefinitionAnalyser.Definition> generatedDefinitions(
+				final Vertex vertex) {
+			Set<ReachingDefinitionAnalyser.Definition> genSet = new HashSet<>();
+			GremlinPipeline<Vertex, Vertex> pipe = new GremlinPipeline<>();
+			pipe.start(vertex)
+			    .out("WRITE")
+			    .out("BELONGS_TO")
+			    .in("BELONGS_TO");
+			for (Vertex register : pipe) {
+				String registerName = register.getProperty("name");
+				genSet.add(new ReachingDefinitionAnalyser.Definition(vertex,
+						registerName));
+			}
+			return genSet;
+		}
+
+		public static Set<ReachingDefinitionAnalyser.Definition> killedDefinitions(
+				final Vertex vertex) {
+			Set<ReachingDefinitionAnalyser.Definition> killSet = new HashSet<>();
+			GremlinPipeline<Vertex, Edge> pipe = new GremlinPipeline<>();
+			pipe.start(vertex)
+			    .out("WRITE")
+			    .out("BELONGS_TO")
+			    .in("BELONGS_TO")
+			    .inE("WRITE");
+			for (Edge writeEdge : pipe) {
+				Vertex genVertex = writeEdge.getVertex(Direction.OUT);
+				Vertex aloc = writeEdge.getVertex(Direction.IN);
+				GremlinPipeline<Vertex, Object> pipe2 = new
+						GremlinPipeline<>();
+				pipe2.start(aloc)
+				     .out("BELONGS_TO")
+				     .in("BELONGS_TO")
+				     .property("name");
+				for (Object identifier : pipe2) {
+					killSet.add(new ReachingDefinitionAnalyser.Definition(
+							genVertex, identifier));
+				}
+			}
+			return killSet;
 		}
 	}
 }
