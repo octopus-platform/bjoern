6ac82e01b96b95e38a03b6f49db94f041cc4ef7f
Markus Lottmann <lottmann86@gmx.de>
2016-10-04 14:15:30 +0200

    Fixed groovy definitions of "reaches" steps.

--- projects/bjoern-lang/src/main/groovy/dataflow.groovy
+++ projects/bjoern-lang/src/main/groovy/dataflow.groovy
@@ -1,15 +1,15 @@
 reachesWith = { args ->
-    _().outE("REACHES").filter { it.aloc.name in args }.inV()
+    _().outE("REACHES").filter { it.aloc in args }.inV()
 }
 
 reachesWithout = { args ->
-    _().outE("REACHES").filter { !(it.aloc.name in args) }.inV()
+    _().outE("REACHES").filter { !(it.aloc in args) }.inV()
 }
 
 revReachesWith = { args ->
-    _().inE("REACHES").filter { it.aloc.name in args }.outV()
+    _().inE("REACHES").filter { it.aloc in args }.outV()
 }
 
 revReachesWithout = { args ->
-    _().inE("REACHES").filter { !(it.aloc.name in args) }.outV()
+    _().inE("REACHES").filter { !(it.aloc in args) }.outV()
 }
