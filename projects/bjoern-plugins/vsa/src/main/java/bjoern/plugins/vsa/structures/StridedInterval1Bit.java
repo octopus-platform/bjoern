a98f7eae3f1af1cf529a8e4a0b643ae1848d2bcf
Alwin Maier <alwin.maier@tu-bs.de>
2016-10-07 13:45:49 +0200

    Small fix.

--- projects/bjoern-plugins/vsa/src/main/java/bjoern/plugins/vsa/structures/StridedInterval1Bit.java
+++ projects/bjoern-plugins/vsa/src/main/java/bjoern/plugins/vsa/structures/StridedInterval1Bit.java
@@ -3,7 +3,7 @@ package bjoern.plugins.vsa.structures;
 public class StridedInterval1Bit extends StridedInterval
 {
 	static final StridedInterval1Bit BOTTOM = new StridedInterval1Bit(1, 1, 0);
-	static final StridedInterval1Bit TOP = new StridedInterval1Bit(1, DataWidth.R64.getMinimumValue(),
+	static final StridedInterval1Bit TOP = new StridedInterval1Bit(1, DataWidth.R1.getMinimumValue(),
 			DataWidth.R1.getMaximumValue());
 
 	protected StridedInterval1Bit(long stride, long lower, long upper)
