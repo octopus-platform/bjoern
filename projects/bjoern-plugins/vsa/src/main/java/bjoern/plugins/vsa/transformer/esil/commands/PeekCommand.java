566744f09afc1efe6e7b67bd958e7e6d4a0265b4
Alwin Maier <alwin.maier@tu-bs.de>
2016-10-18 10:19:21 +0200

    Update peek command.

--- projects/bjoern-plugins/vsa/src/main/java/bjoern/plugins/vsa/transformer/esil/commands/PeekCommand.java
+++ projects/bjoern-plugins/vsa/src/main/java/bjoern/plugins/vsa/transformer/esil/commands/PeekCommand.java
@@ -10,47 +10,46 @@ import bjoern.plugins.vsa.transformer.esil.stack.ESILStackItem;
 import bjoern.plugins.vsa.transformer.esil.stack.ValueSetContainer;
 
 import java.util.Deque;
+import java.util.Set;
 
 public class PeekCommand implements ESILCommand {
 
 	@Override
 	public ESILStackItem execute(
 			Deque<ESILCommand> stack, AbstractEnvironment env) {
-		ValueSet value = stack.pop().execute(stack, env).getValue();
-		StridedInterval bpOffset = getValueOfLocalRegion(value);
-		// Peek from non-local region.
-		if (bpOffset.isBottom()) {
+		ValueSet offsets = getOperand(stack, env).getValue();
+
+		Long spOffset = getSPOffset(offsets);
+		if (spOffset == null) {
 			return new ValueSetContainer(ValueSet.newTop(DataWidth.R64));
 		}
-		ValueSet tmp = env.getRegister("rbp");
-		if (tmp != null) {
-			StridedInterval rbp = getValueOfLocalRegion(tmp);
-			if (rbp.isBottom()) {
-				return new ValueSetContainer(ValueSet.newTop(DataWidth.R64));
-			}
-			StridedInterval spOffset = bpOffset.sub(rbp);
-			if (spOffset.isSingletonSet()) {
-				for (long address : spOffset.values()) {
-					ValueSet data = env.getSPVariable(address);
-					if (data != null) {
-						return new ValueSetContainer(data);
-					}
-				}
-			}
+		ValueSet value = env.getBPVariable(spOffset);
+		if (value == null) {
+			return new ValueSetContainer(ValueSet.newTop(DataWidth.R64));
+		} else {
+			return new ValueSetContainer(value);
 		}
-		return new ValueSetContainer(ValueSet.newTop(DataWidth.R64));
 	}
 
-	private StridedInterval getValueOfLocalRegion(ValueSet valueSet) {
-		if (valueSet.isTop()) {
-			return StridedInterval.getTop(valueSet.getDataWidth());
+	private Long getSPOffset(final ValueSet offsets) {
+		Set<MemoryRegion> regions = offsets.getRegions();
+		if (regions.size() != 1) {
+			return null;
 		}
-		for (MemoryRegion region : valueSet.getRegions()) {
-			if (region instanceof LocalRegion) {
-				StridedInterval interval = valueSet.getValueOfRegion(region);
-				return interval;
-			}
+		MemoryRegion region = regions.iterator().next();
+		if (!(region instanceof LocalRegion)) {
+			return null;
 		}
-		return StridedInterval.getBottom(valueSet.getDataWidth());
+		StridedInterval interval = offsets.getValueOfRegion(region);
+		if (!interval.isSingletonSet()) {
+			return null;
+		}
+		return interval.values().iterator().next();
 	}
+
+	protected ESILStackItem getOperand(
+			Deque<ESILCommand> stack, AbstractEnvironment env) {
+		return stack.pop().execute(stack, env);
+	}
+
 }
