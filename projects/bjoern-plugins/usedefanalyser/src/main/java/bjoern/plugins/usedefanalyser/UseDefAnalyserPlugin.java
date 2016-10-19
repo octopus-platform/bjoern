64e213a586440abcf63b00dfce51a0e87e37623a
Alwin Maier <alwin.maier@tu-bs.de>
2016-10-19 11:36:55 +0200

    Add logging message.

--- projects/bjoern-plugins/usedefanalyser/src/main/java/bjoern/plugins/usedefanalyser/UseDefAnalyserPlugin.java
+++ projects/bjoern-plugins/usedefanalyser/src/main/java/bjoern/plugins/usedefanalyser/UseDefAnalyserPlugin.java
@@ -5,13 +5,18 @@ import bjoern.pluginlib.structures.BasicBlock;
 import bjoern.pluginlib.structures.Function;
 import com.tinkerpop.blueprints.impls.orient.OrientGraphNoTx;
 import octopus.lib.plugintypes.OrientGraphConnectionPlugin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UseDefAnalyserPlugin extends OrientGraphConnectionPlugin {
+	private Logger logger = LoggerFactory.getLogger(UseDefAnalyser.class);
+
 	@Override
 	public void execute() throws Exception {
 		OrientGraphNoTx graph = orientConnector.getNoTxGraphInstance();
 		UseDefAnalyser analyzer = new UseDefAnalyser();
 		for (Function function : LookupOperations.getFunctions(graph)) {
+			logger.info(function.toString());
 			for (BasicBlock block : function.basicBlocks()) {
 				analyzer.analyse(block);
 			}
